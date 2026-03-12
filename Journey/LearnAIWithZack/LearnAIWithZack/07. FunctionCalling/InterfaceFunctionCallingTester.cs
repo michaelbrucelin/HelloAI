9157b823a4476aa7bf06ef16fb912895f97e091f
michaelbrucelin <michaelbrucelin@gmail.com>
2026-03-12 12:00:43 +0800

    20260312-120042

---
+++ Journey/LearnAIWithZack/LearnAIWithZack/07. FunctionCalling/InterfaceFunctionCallingTester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAIWithZack._07._FunctionCalling
+{
+    public interface InterfaceFunctionCallingTester
+    {
+        public Task Test();
+    }
+}
